4dba12d42af6f18eeae99b27598625b0e1a68808
marcinoles87 <marcinoles87@gmail.com>
2023-05-01 19:42:56 +0200

    add structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 
-function App() {
+import Item from './components/Item'
+
+ const App = () => {
   return (
     <div className="App">
       <h1>Shopping list App</h1>
+      <Item></Item>
     </div>
   );
 }
