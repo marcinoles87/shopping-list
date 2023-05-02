4de190d124adc0cc4455a981ba6012bcce9ac381
marcinoles87 <marcinoles87@gmail.com>
2023-05-02 17:16:17 +0200

    add structure

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,10 +6,14 @@ const Item = (props) => {
     let {value} = props
   
 
+    const [pieces , setPieces] = useState(0)
 
     return(
         <div className='item-card'>
            <p>{value}</p>
+           <button>+</button>
+           <button>-</button>
+           <p>Pices : {pieces} </p>
         </div>
     )
 }
